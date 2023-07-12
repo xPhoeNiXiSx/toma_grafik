f4b20782492cafc6bab79c735ced5a3e1faf7f34
xPhoeNiXiSx <fraquet.thomas91@gmail.com>
2023-07-12 10:13:34 +0200

    toma

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'toma-grafik',
+    appId: '1:1089650008428:web:fd9ea518194dc61532d349',
+    databaseURL: 'https://toma-grafik-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'toma-grafik.appspot.com',
+    apiKey: 'AIzaSyAp2Lo8jZST_aNrL1Pxh2kOvhwnNiiW_Ao',
+    authDomain: 'toma-grafik.firebaseapp.com',
+    messagingSenderId: '1089650008428',
+    measurementId: 'G-RYXD41ZJPZ',
+  }
 };
 
 /*
