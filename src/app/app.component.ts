211f4005a00d8901493286ecbf7997cac97cf5b7
tfraquet <tfraquet@a5sys.com>
2023-07-19 14:07:42 +0200

    MAJ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { fadeIn, fadeInOutPage, fadeOut } from './app.animations';
 import { ChildrenOutletContexts, RouterOutlet } from '@angular/router';
 
@@ -8,14 +8,42 @@ import { ChildrenOutletContexts, RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss'],
   animations: [fadeIn, fadeOut, fadeInOutPage],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   isShown = false;
+  @ViewChild('cursor') //Get element #cursor
+  public cursor?: ElementRef;
+  @ViewChild('cursorDot') //Get element #cursorDot
+  public cursorDot?: ElementRef;
 
   constructor(
-    private contexts: ChildrenOutletContexts
+    private contexts: ChildrenOutletContexts,
+    private renderer: Renderer2,
   ) {}
 
+  @HostListener('mousemove', ['$event'])
+  moveCursor(event: MouseEvent) {
+    let x = event.clientX;
+    let y = event.clientY;
+    // console.log(x, y);
+    if (this.cursor && this.cursorDot) {
+      this.renderer.setStyle(this.cursor.nativeElement, 'transform', 'translate3d(' + x + 'px,' + y +'px, 0)');
+      this.renderer.setStyle(this.cursorDot.nativeElement, 'transform', 'translate3d(' + x + 'px,' + y +'px, 0)');
+    }
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    console.log('test');
+    // if (this.cursor) {
+    //   this.cursor.nativeElement.classList('hover');
+    // }
+  }
+
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    const child = document.createElement("div").classList.add("cursor");
+  }
 
   fadeInOut(): void {
     this.isShown = !this.isShown;
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import "./scroll";
 
 @Component({
   selector: 'app-about',
