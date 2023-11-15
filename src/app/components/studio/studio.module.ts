5257c5b0e661fdc07a198879900e5df6e1b6108e
xPhoeNiXiSx <fraquet.thomas91@gmail.com>
2023-11-15 23:21:41 +0100

    MAJ Parrallax directive

--- src/app/components/studio/studio.module.ts
+++ src/app/components/studio/studio.module.ts
@@ -7,6 +7,7 @@ import { StudiohomeComponent } from './studiohome/studiohome.component';
 import { ArchiComponent } from './archi/archi.component';
 import { ConceptComponent } from './archi/concept/concept.component';
 import { ArchiHeroComponent } from './archi/hero/archi-hero.component';
+import { ParallaxDirective } from 'src/app/directives/parallax.directive';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { ArchiHeroComponent } from './archi/hero/archi-hero.component';
     StudiohomeComponent,
     ArchiComponent,
     ConceptComponent,
-    ArchiHeroComponent
+    ArchiHeroComponent,
+    ParallaxDirective
   ],
   imports: [
     CommonModule,
